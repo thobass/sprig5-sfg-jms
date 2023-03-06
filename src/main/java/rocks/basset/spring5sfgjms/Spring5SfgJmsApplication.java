31d259229d01f1754afa43d05e48e121f9bdc0af
Thomas Basset <thomas@basset.rocks>
2023-03-06 08:32:29 +0100

    Init project

---
+++ src/main/java/rocks/basset/spring5sfgjms/Spring5SfgJmsApplication.java
@@ -0,0 +1,13 @@
+package rocks.basset.spring5sfgjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5SfgJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5SfgJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rocks/basset/spring5sfgjms/Spring5SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.basset.spring5sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
