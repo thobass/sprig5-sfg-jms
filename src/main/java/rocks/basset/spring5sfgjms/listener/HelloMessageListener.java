d6741b5837dc0f57be0d86b31e35d875c000d845
Thomas Basset <thomas@basset.rocks>
2023-03-06 09:43:56 +0100

    Send and Receive of JMS Messages

--- src/main/java/rocks/basset/spring5sfgjms/listener/HelloMessageListener.java
+++ src/main/java/rocks/basset/spring5sfgjms/listener/HelloMessageListener.java
@@ -1,7 +1,9 @@
 package rocks.basset.spring5sfgjms.listener;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,15 +11,32 @@ import org.springframework.stereotype.Component;
 import rocks.basset.spring5sfgjms.config.JmsConfig;
 import rocks.basset.spring5sfgjms.model.HelloWorldMessage;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message){
-        log.info("I Got a message");
-        log.info("{}", helloWorldMessage);
+//        log.info("I Got a message");
+//        log.info("{}", helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("world=")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
--- src/main/java/rocks/basset/spring5sfgjms/sender/HelloSender.java
+++ src/main/java/rocks/basset/spring5sfgjms/sender/HelloSender.java
@@ -1,13 +1,19 @@
 package rocks.basset.spring5sfgjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import rocks.basset.spring5sfgjms.config.JmsConfig;
 import rocks.basset.spring5sfgjms.model.HelloWorldMessage;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @Slf4j
@@ -16,6 +22,7 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate =  2000)
     public void sendMessage(){
@@ -28,5 +35,32 @@ public class HelloSender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
         log.info("I'm sending a message...");
+    }
+
+    @Scheduled(fixedRate =  2000)
+    public void sendAndReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello world")
+                .build();
+
+        Message receiveMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RECEIVE_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "rocks.basset.spring5sfgjms.model.HelloWorldMessage");
+                    log.info("Sending hello");
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("kaboom");
+                }
+            }
+        });
+
+        log.info("Message sent.");
+        log.info("{}", receiveMessage.getBody(String.class));
     }
 }
